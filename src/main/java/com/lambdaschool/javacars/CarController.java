c2d7c4f997fec1cb107206c41880e644c37850c5
Eddy <edward.gonzalez9708@gmail.com>
2019-02-20 16:53:34 -0500

    Created a get endpoint in the Car Controller class for the route /cars/id/{id}.

---
+++ src/main/java/com/lambdaschool/javacars/CarController.java
@@ -0,0 +1,50 @@
+package com.lambdaschool.javacars;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class CarController {
+    private final CarRepository carrepos;
+    private final RabbitTemplate rt;
+
+    public CarController(CarRepository carrepos, RabbitTemplate rt) {
+        this.carrepos = carrepos;
+        this.rt = rt;
+    }
+
+    // ### GET
+
+    // /cars/id/{id}
+    // returns the car based of of id
+
+    @GetMapping("/cars/id/{id}")
+    public Car findCar(@PathVariable Long id) {
+        return carrepos.findById(id)
+                .orElseThrow(() ->
+                        new CarNotFoundException("Car record with id " + id + " does not exist."));
+    }
+
+    // /cars/year/{year}
+    // returns a list of cars of that year model
+
+    // /cars/brand/{brand}
+    // returns a list of cars of that brand
+    // This gets logged with a message of "search for {brand}". So put the brand of the car that was searched in the message itself.
+
+    // ### POST
+
+    // /cars/upload
+    // loads multiple sets of data from the RequestBody
+    // This gets logged with a message of "Data loaded"
+
+    // ### DELETE
+
+    // /cars/delete/{id}
+    // deletes a car from the list based off of the id
+    // This gets logged with a message of "{id} Data deleted". So, put the id of the car that got deleted in the message itself.
+}
